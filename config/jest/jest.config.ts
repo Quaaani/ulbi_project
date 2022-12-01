22450740fe9ba02981c9e6d4e6251fc5731b001a
Личный <lovi.kuanyshev@gmail.com>
2022-12-01 20:27:39 +0600

    [1.11.71] Implemented Jest Unit HTML Reports

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -52,6 +52,20 @@ export default {
     __PROJECT__: 'jest',
   },
 
+  // Use this configuration option to add custom reporters to Jest
+  reporters: [
+    'default',
+    [
+      'jest-html-reporters',
+      {
+        publicPath: '<rootDir>/reports/unit',
+        filename: 'report.html',
+        openReport: true,
+        inlineSource: true,
+      },
+    ],
+  ],
+
   // Indicates whether the coverage information should be collected while executing the test
   // collectCoverage: false,
 
@@ -110,9 +124,6 @@ export default {
 
   // Run tests from one or more projects
   // projects: undefined,
-
-  // Use this configuration option to add custom reporters to Jest
-  // reporters: undefined,
 
   // Automatically reset mock state before every test
   // resetMocks: false,
